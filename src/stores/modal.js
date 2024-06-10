591766880325c009a7de3f00afaacfc4812949d6
Salman <salmanpatni92@gmail.com>
2024-06-10 14:13:01 +0500

    Add pinia store for modal.

---
+++ src/stores/modal.js
@@ -0,0 +1,10 @@
+import { computed, ref } from 'vue'
+import { defineStore } from 'pinia'
+
+export const useModalStore = defineStore('modal', () => {
+  const isOpen = ref(false)
+
+  const hiddenClass = computed(() => (!isOpen.value ? 'hidden' : ''))
+
+  return { isOpen, hiddenClass }
+})
